22eeaecb538f0642f0511bb5fbc4b5f21d856ba4
ljeanner <jeanneret.lucile@gmail.com>
2017-02-10 14:46:03 +0100

    Ajoue Currency

--- js/api.js
+++ js/api.js
@@ -47,3 +47,17 @@ document.getElementById("peerinfo").innerHTML = jsonPretty;
 };
 xmlhttp.open("GET", "http://bitcoin.mubiz.com/peerinfo", true);
 xmlhttp.send();
+
+
+/*curency*/
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+if (this.readyState == 4 && this.status == 200) {
+var myObj = this.responseText;
+var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+document.getElementById("currency").innerHTML = jsonPretty;
+}
+};
+xmlhttp.open("GET", " http(s)://api.coindesk.com/v1/bpi/currentprice.json", true);
+xmlhttp.send();
