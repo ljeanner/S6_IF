ef8d6d570ae0200f3717f22f5a318547d55f57ed
ljeanner <jeanneret.lucile@gmail.com>
2017-04-13 12:48:02 +0200

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -7,10 +7,14 @@ function fonctionRequeteApi(url,elementID)
             var myObj = this.responseText;
             var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
             document.getElementById(elementID).innerHTML = jsonPretty;
+            
+            output(jsonPretty,elementID);
+            output(syntaxHighlight(jsonPretty),elementID);
         }
     };
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
+
 }
 
 /* fonction quon appel pour charger les API*/
@@ -26,9 +30,51 @@ function homePageLoading() {
 
 function SendData()
 {
+    /*name */
     var api=document.getElementById("select_api").value;
     var adresse = "https://bitcoin.mubiz.com/"
     var url= adresse.concat(api);
     fonctionRequeteApi(url,"resultat");
+    
+    /*hash*/
+    var hash =document.getElementById("select_hash").value;
+    var adresse_hash = "https://api.blockcypher.com/v1/btc/main/blocks/"
+    var url_hash= adresse_hash.concat(hash);
+    fonctionRequeteApi(url_hash,"resultathash");
+    
+    
+    /*index*/
+    var index =document.getElementById("select_index").value;
+    var get_index = "/block_index/"
+    var tmp = get_index.concat(index);
+    var url_index = adresse.concat(tmp);
+    url_index = url_index.concat("/");
+    fonctionRequeteApi(url_index,"resultatindex")
 }
+function output(inp, elementID) {
+    //document.body.appendChild(document.createElement('pre')).innerHTML = inp;
+     document.getElementById(elementID).innerHTML = inp;
+}
+
+function syntaxHighlight(json) {
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
+
+
+
 
